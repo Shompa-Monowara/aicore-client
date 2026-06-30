3bb89cdcf58caa09916c2d2700849c1cee840dc6
shompadmbd-sketch <shompadmbd@gmail.com>
2026-06-30 12:38:01 +0600

    add some featured

--- src/proxy.js
+++ src/proxy.js
@@ -18,11 +18,11 @@
 const role = (user.role || "user").toLowerCase();
 const plan = (user.plan || "free").toLowerCase();
 
-  //  if (pathname.startsWith("/payment") && plan === "premium") {
+   if (pathname.startsWith("/payment") && plan === "premium") {
     
-  //   const dashboardRoute = `/dashboard/${role}/profile`;
-  //  return NextResponse.redirect(new URL(dashboardRoute, request.url));
-  //  }
+     const dashboardRoute = `/dashboard/${role}/profile`;
+    return NextResponse.redirect(new URL(dashboardRoute, request.url));
+   }
 
   
   if (pathname.startsWith("/dashboard/admin") && role !== "admin") {
